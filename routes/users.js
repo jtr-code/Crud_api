bfb8293f0a3ef6cbd66c862a448544a6e3f3c2a5
jtr-code <jtrtricks@gmail.com>
2022-11-30 21:40:57 +0530

    [Added] id for user

--- routes/users.js
+++ routes/users.js
@@ -3,28 +3,17 @@ const router = express.Router();
 
 //all routes here are starting with /users
 
-let users = [
-    {
-        firstName: "Jishnu",
-        lastName: "t raj",
-        age: 22
-    },
-    {
-        firstName: "John",
-        lastName: "Dane",
-        age: 20
-    },
-];
+let users = [];
 
 router.get("/", (req, res) => {
     res.send(users);
 });
- 
+
 router.post("/", (req, res) => {
-    
     const user = req.body;
-    users.push(user);
- 
+    
+    users.push({ ...user, id: Date.now() });
+
     res.send(`Added the user ${user.firstName} ${user.lastName} `);
 });
 
