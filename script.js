7788a249db7132f323147d194a59972d05a68c66
jtr-code <jtrtricks@gmail.com>
2022-11-30 20:44:22 +0530

    Initial stage

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const userRoutes = require("./routes/users");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/users",userRoutes)
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Hello from homepage")
+    res.send("Hello from homepage");
 });
 
 app.listen(PORT, () => {
