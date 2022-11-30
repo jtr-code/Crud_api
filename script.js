e899489defe09a149354a861bc5b7fcc2a7ae8a3
jtr-code <jtrtricks@gmail.com>
2022-11-30 20:23:43 +0530

    Initial build

---
+++ script.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const userRoutes = require("./routes/users");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/users",userRoutes)
+
+app.get("/", (req, res) => {
+    res.send("Hello from homepage")
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
